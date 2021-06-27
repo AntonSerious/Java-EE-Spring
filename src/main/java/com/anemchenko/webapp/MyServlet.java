026d87b1f63dea11d737bdf5732b9d3df2ba4bc8
anemchenko <anemchenko@nes.ru>
2021-06-27 17:41:31 +0300

    first commit

---
+++ src/main/java/com/anemchenko/webapp/MyServlet.java
@@ -0,0 +1,32 @@
+package com.anemchenko.webapp;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyServlet extends HttpServlet {
+    private static Logger logger = LoggerFactory.getLogger(MyServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().printf("<html><body>");
+        for (int i = 0; i < 10; i++) {
+            resp.getWriter().printf("<h1>" + i + "</h1>");
+        }
+        resp.getWriter().printf("</body></html>");
+    }
+
+    @Override
+    public void destroy() {
+        logger.debug("Destroy");
+    }
+
+    @Override
+    public void init() throws ServletException {
+        logger.debug("Init");
+    }
+}
